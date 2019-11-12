4bbf18afef840b9e8b6facd672c576110a7477cb
rajustin <rajustin10@hotmail.com>
2019-11-12 10:10:43 -1000

    lab 13

--- Lab13/info_server_Ex3.js
+++ Lab13/info_server_Ex3.js
@@ -1,6 +1,9 @@
 var express = require('express'); // calls for the code to look for the express file
 var app = express(); // makes express into app
 var myParser = require("body-parser");
+var fs = require('fs');
+var data = require('./public/product_data.js');
+var products = data.products;
 
 app.all('*', function (request, response, next) {
     console.log(request.method + ' to ' + request.path); 
@@ -9,15 +12,20 @@ app.all('*', function (request, response, next) {
 
 app.use(myParser.urlencoded({ extended: true }));
 app.post("/process_form", function (request, response) {
-   let POST = request.body;
-   if (typeof POST['quantity_textbox'] != 'undefined'){
-    displayPurchase(POST, response);
-} 
+    process_quantity_form(request.body, response);
+
 });
 
 app.use(express.static('./public'));
 app.listen(8080, () => console.log(`listening on port 8080`));
 
+function process_quantity_form (POST, response) {
+if (typeof POST['quantity_textbox'] != 'undefined'){
+    displayPurchase(POST, response);
+    let model = products[0]['model'];
+    let model_price = products[0]['price'];
+    }
+} 
 
 function NotNegativeNumber(q, returnErrors = false) {
     errors = []; // assume no errors at first
@@ -29,9 +37,13 @@ function NotNegativeNumber(q, returnErrors = false) {
 
 function displayPurchase(POST, response){
     q = POST['quantity_textbox'];
-    if(NotNegativeNumber(q)) {
-        response.send(`Thank you for purchasing ${q} things!`);
-     } else {
-     response.send(`${q} is not a quantity! Press the back button and try again.`);
+    if (typeof POST['quantity_textbox'] != 'undefined') {
+        let q = POST['quantity_textbox'];
+        if (NotNegativeNumber(q)) {
+            var contents = fs.readFileSync('./views/display_quantity_template.view', 'utf8');
+            response.send(eval('`' + contents + '`')); // render template string
+        } else {
+            response.send(`${q} is not a quantity!`);
+        }
     }
 }
