835b87a7312a47e13057720e77cf8ff43d24c1ab
rajustin <rajustin10@hotmail.com>
2019-11-22 14:24:26 -1000

    fixed

--- Ra_Justin_Assigment1/public/product_data.js
+++ Ra_Justin_Assigment1/public/product_data.js
@@ -5,27 +5,27 @@ var products =
     //product 1
    { "image": "https://i.pinimg.com/474x/8a/e8/9a/8ae89a90e576104183339ca5f042757e.jpg", 
     "product": "1 Star Ball",
-     "price": "$40.00"
+     "price": 40.00
     },
     //product 2
    { "image": "https://res.cloudinary.com/teepublic/image/private/s--t8M2-x_4--/t_Preview/b_rgb:fffffe,c_limit,f_jpg,h_630,q_90,w_630/v1481686426/production/designs/946204_1.jpg", 
     "product": "2 Star Ball", 
-    "price": "$75.00"
+    "price": 75.00
     },
     //product 3
     { "image": "https://i.pinimg.com/originals/0f/a1/80/0fa180733bb42a49728bc76b3bb7bcee.jpg", 
     "product": "3 Star Ball", 
-    "price": "$35.00" 
+    "price": 35.00
     },
     //product 4
     { "image": "https://i.pinimg.com/474x/59/85/60/5985603434b7dab2038dd5026b9218e1.jpg", 
         "product": "4 Star Ball", 
-        "price": "$45.00" 
+        "price": 45.00
         },
     //product 5
     { "image": "https://res.cloudinary.com/teepublic/image/private/s--Z3AATEf6--/t_Preview/b_rgb:fffffe,c_limit,f_jpg,h_630,q_90,w_630/v1481688418/production/designs/946328_1.jpg", 
         "product": "5 Star Ball", 
-        "price": "$10.00 "
+        "price": 10.00
         }
 ];
 
